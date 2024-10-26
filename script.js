47d1175d35fb1f8b43026a8dfdb0a1f50c85b6fa
Ophélia <ochheak@gmail.com>
2024-10-26 22:14:37 +0200

    add cv and tableau synthèse

---
+++ script.js
@@ -0,0 +1,12 @@
+function togglePDFLinks() {
+    const pdfLinks = document.getElementById("pdfLinks");
+    const button = document.querySelector(".toggle-btn");
+
+    if (pdfLinks.style.display === "none" || pdfLinks.style.display === "") {
+        pdfLinks.style.display = "block";
+        button.textContent = "Masquer";
+    } else {
+        pdfLinks.style.display = "none";
+        button.textContent = "Afficher";
+    }
+}
