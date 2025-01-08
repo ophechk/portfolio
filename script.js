58e00a5f43590a03bde5a215b8eaae7264e79bd9
Ophélia <ochheak@gmail.com>
2025-01-08 23:00:20 +0100

    delete images, modify e4 tableau, add carrousel

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ function togglePDFLinks() {
     }
 }
 
+/*carrousel*/
 
+const track = document.querySelector('.carousel-track');
+        const slides = Array.from(track.children);
+        const nextButton = document.querySelector('.next');
+        const prevButton = document.querySelector('.prev');
+
+        let currentSlideIndex = 0;
+
+        function updateCarousel() {
+            const slideWidth = slides[0].getBoundingClientRect().width;
+            track.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+        }
+
+        nextButton.addEventListener('click', () => {
+            currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+            updateCarousel();
+        });
+
+        prevButton.addEventListener('click', () => {
+            currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+            updateCarousel();
+        });
+
+        window.addEventListener('resize', updateCarousel);
